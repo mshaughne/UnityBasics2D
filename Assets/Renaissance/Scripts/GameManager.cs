3d50ef8dc02c66e0f023d4af53f85db6e9c407f5
Michael <mshaughne@ltu.edu>
2025-04-25 17:54:57 -0400

    Renaissance collectible video lecture

--- Assets/Renaissance/Scripts/GameManager.cs
+++ Assets/Renaissance/Scripts/GameManager.cs
@@ -1,16 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// an instance of the gamemanager that becomes a static
+    /// </summary>
+    public static GameManager Instance;
+    /// <summary>
+    /// the number of coins the player has obtained
+    /// </summary>
+    public int coinsObtained = 0;
+    /// <summary>
+    /// the UI asset for the amount of coins we're holding
+    /// </summary>
+    public TextMeshProUGUI coinsText;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        coinsText.text = "Coins = " + coinsObtained;
+    }
+
     void Update()
     {
         // if the player presses Escape
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // close the game
-            Application.Quit();
+            // switch to the main menu
+            SceneManager.LoadScene("MainMenuScene");
         }
+    }
+
+    public void ObtainCoin()
+    {
+        coinsObtained++;
+        Debug.Log("Coins = " +  coinsObtained);
+
+        coinsText.text = "Coins = " + coinsObtained;
     }
 }
