75dd7348a05e08a864f69c3f011e020a333f8b7e
Michael <mshaughne@ltu.edu>
2025-05-12 21:54:49 -0400

    Modified Southfield SFX system

--- Assets/Southfield/Scripts/DefeatEnemy.cs
+++ Assets/Southfield/Scripts/DefeatEnemy.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class DefeatEnemy : MonoBehaviour
 {
     public GameObject deathParticle;
+    public AudioClip deathSound;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
+            AudioSource.PlayClipAtPoint(deathSound, transform.parent.position, 1f);
             Instantiate(deathParticle, transform.parent.position, Quaternion.identity);
             collision.gameObject.GetComponent<PlayerMove>().Bounce();
             Destroy(transform.parent.gameObject);
